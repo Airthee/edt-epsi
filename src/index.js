5d8dbd751f88d0d430c9a652e6c7763945c4b7c6
Airthee <13355624+Airthee@users.noreply.github.com>
2019-09-30 18:56:48 +0200

    First commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import moment from 'moment';
+
+const url = new URL(window.location.href);
+
+// Retrieve student id and date
+const userId = url.searchParams.get('id') || 'raphael.tison';
+const date = url.searchParams.get('date') || moment().format('MM/DD/YYYY');
+
+// Make redirection
+const urlRedirect = `https://edtmobiliteng.wigorservices.net/WebPsDyn.aspx?action=posEDTBEECOME&serverid=C&Tel=${userId}&date=${date}`;
+console.log(`Redirecting to ${urlRedirect}`);
+window.location.href = urlRedirect;
